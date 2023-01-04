f6a6d718f562bbd1fb5b5d0be9be311cdbab77bf
Vilanstu <ni426086@gmail.com>
2023-01-03 19:06:53 -0800

    Task 4

---
+++ 0x00-hello_world/4puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like holding a multilingual puzzle")
+	return (0);
+
+}	
